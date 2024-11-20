999e970ce2712c6bf0c0d0bce40d132ac3a4408b
lain561 <mckissicethan@gmail.com>
2024-11-19 21:47:11 -0500

    init

---
+++ Railroad.java
@@ -0,0 +1,24 @@
+/* Ethan McKissic
+   Dr. Steinberg
+   COP3503 Fall 2024
+   Programming Assignment 5
+*/
+
+public class Railroad 
+{
+
+    public Railroad(int tracks, String fileName)
+    {
+
+
+        
+    }
+
+    
+    public String buildRailroad()
+    {
+
+    }
+
+
+}
